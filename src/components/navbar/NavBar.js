86878dc36d635363817204afe6e33634115ceb50
gaston.ara <gaston.ara.o@gmail.com>
2021-06-29 22:16:55 -0300

    Navbar hecho con CartWidget incluido

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import CartWidget from './CartWidget.js'
+
+function NavBar() {
+    return (
+        <div className="fixed-nav">
+             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Navbar.Brand href="/">ModernClix</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="/">Inicio</Nav.Link>
+                    <Nav.Link href="/#catalogo">Catálogo</Nav.Link>
+                    <Nav.Link href="/#contacto">Contacto</Nav.Link>
+                </Nav>
+                <Nav>
+                    <Nav.Link href="/Login">Iniciar sesion</Nav.Link>
+                    <Nav.Link href="/Cart"><CartWidget /></Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+        </div>
+       
+    );
+
+}
+
+export default NavBar;
